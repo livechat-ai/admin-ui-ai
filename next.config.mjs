ed5b182893302b2e03ea08c5677befa0cdf03f86
your-name <your-email@gmail.com>
2026-02-14 15:03:19 +0800

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://livechat-be:3310/api/:path*", // Proxy to backend service
+            },
+        ];
+    },
 };
 
 export default nextConfig;
